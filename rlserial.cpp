c810c27ae8d04aef59f4b50995e36b2c73802a3c
IstrateMihai <mihallex14@gmail.com>
2022-06-06 02:07:07 +0300

    update

--- rlserial.cpp
+++ rlserial.cpp
@@ -3,14 +3,14 @@
 void SendSerial(char* name ,char *data)
 {
  
-Serial.print(strcat(name," "));
+Serial.print(name+" ");
   Serial.print(data);
  Serial.println(); 
 }
 void SendSerial(char* name ,double data)
 {
- 
-Serial.print(strcat(name," "));
+  Serial.println(); 
+  Serial.print(name+" ");
   Serial.print(data);
  Serial.println(); 
 }
